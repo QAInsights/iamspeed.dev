3d88814e0303865b107fb344196ac48d634abe35
NaveenKumar Namachivayam <catch.nkn@gmail.com>
2026-06-13 00:11:32 -0400

    feat: add sitemap integration and configure site domain with robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
+  site: 'https://iamspeed.dev',
   output: 'static',
-  integrations: [preact()],
+  integrations: [preact(), sitemap()],
 });
